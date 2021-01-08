9373f1a339d603dfff830afebb4f7230f259855c
Jerrod Styrk <jstyrk@citadel.edu>
2021-01-08 11:24:30 -0800

    Add Migrations and build out Engineer index and controller

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,23 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+    public class EngineersController : Controller
+    {
+        private readonly FactoryContext _db;
+        public EngineersController(FactoryContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_db.Engineers.ToList());
+        }
+    }
+}
